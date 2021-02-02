0166e67a7225139d598dc8d1d1aa97c8d14d872b
Ming-der Wang <mingderwang@gmail.com>
2021-02-02 12:22:10 +0800

    ok for ipfs

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 // create context menu item
 chrome.contextMenus.create({
   id: "main_page",
-  title: "Open Main Page", 
+  title: "開發者網頁",
   onclick: () => {
     chrome.tabs.create({  
       url: chrome.runtime.getURL("src/main.html")
